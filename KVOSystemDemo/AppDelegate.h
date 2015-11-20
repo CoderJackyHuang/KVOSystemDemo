7941eff6a170218aea7bbea8b7a16f44672fb159
huangyibiao <huangyibiao@huangyibiaodeMacBook-Pro.local>
2015-11-20 23:58:32 +0800

    init project

---
+++ KVOSystemDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVOSystemDemo
+//
+//  Created by huangyibiao on 15/11/20.
+//  Copyright © 2015年 huangyibiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVOSystemDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVOSystemDemo
+//
+//  Created by huangyibiao on 15/11/20.
+//  Copyright © 2015年 huangyibiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
